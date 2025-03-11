fd402c9c4af214e734b202cfad15b0321caf306c
MonsterDK <daniel.monstr.kv@gmail.com>
2025-03-11 20:56:39 +0600

    add style animation @szhsin/react-accordion

--- src/components/AccordionWrapperImg.tsx
+++ src/components/AccordionWrapperImg.tsx
@@ -57,7 +57,7 @@ const AccordionWrapperImg: FC = () => {
                     {el.title}
                   </p>
 
-                  <span className="transition-transform duration-300">
+                  <span className="transition-transform duration-[250ms]">
                     <svg
                       width="20"
                       height="20"
@@ -65,7 +65,7 @@ const AccordionWrapperImg: FC = () => {
                       fill="none"
                       xmlns="http://www.w3.org/2000/svg"
                       //   className={`transition-transform duration-100 ${state.isEnter ? "scale-y-[1]" : "-scale-y-[1]"}`}
-                      className={`transition-transform duration-300 transform ${state.isEnter ? "rotate-180" : "rotate-0"}`}
+                      className={`transition-transform duration-[250ms] transform ${state.isEnter ? "rotate-180" : "rotate-0"}`}
                     >
                       <path
                         d="M9.54742 6.72513L2.90992 13.6914C2.79912 13.8076 2.7373 13.962 2.7373 14.1226C2.7373 14.2832 2.79912 14.4376 2.90992 14.5539L2.91742 14.5614C2.97114 14.6179 3.0358 14.6629 3.10746 14.6937C3.17913 14.7245 3.25631 14.7403 3.3343 14.7403C3.41229 14.7403 3.48947 14.7245 3.56113 14.6937C3.6328 14.6629 3.69746 14.6179 3.75117 14.5614L10.0012 8.00138L16.2487 14.5614C16.3024 14.6179 16.367 14.6629 16.4387 14.6937C16.5104 14.7245 16.5876 14.7403 16.6655 14.7403C16.7435 14.7403 16.8207 14.7245 16.8924 14.6937C16.964 14.6629 17.0287 14.6179 17.0824 14.5614L17.0899 14.5539C17.2007 14.4376 17.2625 14.2832 17.2625 14.1226C17.2625 13.962 17.2007 13.8076 17.0899 13.6914L10.4524 6.72513C10.3941 6.66386 10.3238 6.61509 10.2461 6.58176C10.1683 6.54843 10.0845 6.53125 9.99992 6.53125C9.9153 6.53125 9.83156 6.54843 9.75378 6.58176C9.676 6.61509 9.6058 6.66386 9.54742 6.72513Z"
