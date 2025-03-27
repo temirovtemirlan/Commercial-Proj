bc612e2821fdfea8772582753641989a435b13b0
MonsterDK <daniel.monstr.kv@gmail.com>
2025-03-27 18:12:30 +0600

    fix version

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,6 @@ import { AppRouter } from "routes";
 import { store } from "store";
 
 import "styles/index.scss";
-import "fusion/i18n/index.ts";
 
 createRoot(document.getElementById("root")!).render(
   <Suspense fallback={<h1>Loading...</h1>}>
